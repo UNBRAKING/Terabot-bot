02980e4b20cce84f5da829a00d34ebc2f66b9f7a
UNBRAKING <code.pbail@gmail.com>
2024-09-02 22:01:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+# API credentials
+API_ID = 26614080
+API_HASH = "7d2c9a5628814e1430b30a1f0dc0165b"
+BOT_TOKEN = "7221307746:AAH9d261LHKQ95WqksDiK9I78XJXXxByL-s"
+
+# Redis configuration
+REDIS_CONFIG = {
+    "HOST": "redis-12345.c16.us-east-1-2.ec2.cloud.redislabs.com",
+    "PORT": 17713,
+    "USERNAME": "default",
+    "PASSWORD": "password",
+    "DATABASE": "NTM"
+}
+
+# Chat ID where videos will be stored
+PRIVATE_CHAT_ID = -1002180025373
+
+# Authentication cookie
+COOKIE = {
+    "COOKIE": "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E="
+}
+# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+
+# List of bot admins
+ADMINS = [5606990991]
